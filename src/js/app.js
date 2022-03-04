d3e8695e25a880d089ded3d6e976456c7ebb4aed
Roman Cazac <romancazac23@icloud.com>
2022-03-04 19:22:28 +0200

    file

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+
+
+// Включить/выключить FLS (Full Logging System) (в работе)
+window['FLS'] = true;
+
+// Подключение основного файла стилей
+import "../scss/style.scss";
+
+
+import "./script.js";
